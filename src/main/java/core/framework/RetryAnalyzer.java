0ca50fa78bc363d25c03fcefe3ba29204950f4fc
krsbhr <krsbhr@xdjpmdd148.its.corp.gwl.com>
2016-12-02 16:10:11 +0530

    Beta version utaf 3.0

---
+++ src/main/java/core/framework/RetryAnalyzer.java
@@ -0,0 +1,34 @@
+package core.framework;
+
+import lib.Stock;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer{
+	 private int retryCount = 0;
+	    private int maxRetryCount = 1;
+	
+	    
+	@Override
+	public boolean retry(ITestResult result) {
+		 if (retryCount < maxRetryCount && Stock.getConfigParam("needRetry").equalsIgnoreCase("true")) {
+	            System.out.println("Retrying test " + result.getName() + " with status "
+	                    + getResultStatusName(result.getStatus()) + " for the " + (retryCount+1) + " time(s).");
+	            retryCount++;
+	            return true;
+	        }
+	        return false;
+	}
+	public String getResultStatusName(int status) {
+ 	String resultName = null;
+ 	if(status==1)
+ 		resultName = "SUCCESS";
+ 	if(status==2)
+ 		resultName = "FAILURE";
+ 	if(status==3)
+ 		resultName = "SKIP";
+		return resultName;
+ }
+
+}
