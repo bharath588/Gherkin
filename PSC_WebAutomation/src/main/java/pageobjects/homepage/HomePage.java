ca929ce0393f9d921e7c3a8cba548be8ab276d7a
smykjn <smykjn@greatwest.com>
2017-03-24 15:45:24 +0530

    Merged

--- PSC_WebAutomation/src/main/java/pageobjects/homepage/HomePage.java
+++ PSC_WebAutomation/src/main/java/pageobjects/homepage/HomePage.java
@@ -2,24 +2,32 @@ package pageobjects.homepage;
 
 import java.lang.reflect.Method;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
+
+import java.util.Arrays;
+
+import java.util.LinkedList;
 
 import lib.Reporter;
 
 import com.aventstack.extentreports.*;
-import com.gargoylesoftware.htmlunit.javascript.host.media.webkitAudioContext;
 
 import lib.DB;
 import lib.Stock;
 import lib.Web;
 
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
@@ -57,6 +65,32 @@ public class HomePage extends LoadableComponent<HomePage>{
 	private WebElement planHeaderInfo;
 	@FindBy(xpath=".//div[@class='ui-growl-message']/span")
 	private WebElement blankPlanNumberErrText;
+	@FindBy(xpath=".//ul[contains(@class,'ui-autocomplete')]/li")
+	private List<WebElement> autoCompleteSuggestionText;
+	@FindBy(xpath="//div[@id='lastLogin']")
+	private WebElement lastLogin;
+	@FindBy(xpath = ".//*[@id='tooltip_todos']")
+	private WebElement toDoButton;
+	@FindBy(xpath = ".//*[@id='tooltip_notifications']")
+	private WebElement notificationButton;
+	@FindBy(xpath="//*[@id='tooltip_compliance']")
+	private WebElement complianceHyperLink;
+	@FindBy(xpath = ".//iframe[@id = 'frameb']")
+	private WebElement iFramePlanB;
+	@FindBy(xpath = ".//*[@id='main']/div/a/following-sibling::div[@class='breadcrumb']")
+	private WebElement tdlBreadcrumb;
+	@FindBy(xpath = ".//*[@id='main']/div/a")
+	private WebElement homeLink;
+	@FindBy(xpath = ".//*[@id='newMenu']/li[1]/a")
+	private WebElement planMenu;
+	@FindBy(xpath = ".//*[@id='newMenu']/li[1]/ul/li[3]/a")
+	private WebElement planAdministration;
+	@FindBy(xpath = ".//*[@id='newMenu']/li[1]/ul/li[3]/ul/li[1]/a")
+	private WebElement planAdministrationToDo;
+	@FindBy(xpath = "html/body/h4[text()='Disbursement requests']")
+	private WebElement disbursementRequest;
+	@FindBy(xpath = ".//*[@id='PlanSummaryMain']//div/ul/li")
+	private List<WebElement> restyledHomeButtons;
 	@FindBy(xpath=".//div[@class='button-row']//button[.='More']")
 	private WebElement moreButton;
 	@FindBy(id="jumpPage")
@@ -87,6 +121,8 @@ public class HomePage extends LoadableComponent<HomePage>{
 	private Method invokeMethodforUserVerification;
 	private String[] userData;
 	private String[] userVeriData;
+	
+	Map<String,String> securityAnsMap=null;
 	ResultSet queryResultSet;
 
 	// userVeriData is optional for HomePage constructor
@@ -104,8 +140,9 @@ public class HomePage extends LoadableComponent<HomePage>{
 
 
 	@Override
-	protected void isLoaded() throws Error {
-		if(!Web.isWebElementDisplayed(weGreeting,true)){
+	protected void isLoaded() throws Error {	
+		if(!Web.isWebElementDisplayed(weGreeting,false)){
+			CommonLib.waitForProgressBar();
 			throw new AssertionError("Plan service center landing page not loaded.");
 		}else{
 			//	Reporter.logEvent(Status.PASS, "Check if Home page is loaded","Home page has loaded successfully",false);
@@ -133,9 +170,11 @@ public class HomePage extends LoadableComponent<HomePage>{
 				loginObj.waitForSuccessfulLogin();
 				//Check if UserVerification Pages appears then performVerification
 				if(Web.isWebElementDisplayed(Web.returnElement(userVeriPg,"EMAIL ADDRESS"))){
-					userVeriData[0] = userVeriPg.getEmailAddressOfuser(Stock.getTestQuery("getEmailaddressQuery"),
+					userVeriData = new String[]{Stock.GetParameterValue("userVerificationEmail"),
+					          getSecurityAnswer((Web.returnElement(userVeriPg, "SECURITYQUESTION")).getText().trim())};
+					/*userVeriData[0] = userVeriPg.getEmailAddressOfuser(Stock.getTestQuery("getEmailaddressQuery"),
 							Stock.GetParameterValue("username"));
-					userVeriData[1] = Stock.GetParameterValue("userVerificationAns");
+					userVeriData[1] = Stock.GetParameterValue("userVerificationAns");*/
 					invokeMethodforUserVerification = userVeriPg.getClass().getDeclaredMethod("performVerification",String[].class);
 					invokeMethodforUserVerification.invoke(userVeriPg,new Object[]{userVeriData});
 				}else{
@@ -172,6 +211,21 @@ public class HomePage extends LoadableComponent<HomePage>{
 		}
 		if (fieldName.trim().equalsIgnoreCase("LOGOUT_LINK")) {
 			return this.logoutLink;
+		}
+		if(fieldName.trim().equalsIgnoreCase("todoLink")){
+			Web.getDriver().switchTo().defaultContent();
+			Web.getDriver().switchTo().frame(iFramePlanB);
+			return this.toDoButton;
+		}
+		if(fieldName.trim().equalsIgnoreCase("notificationLink")){
+			Web.getDriver().switchTo().defaultContent();
+			Web.getDriver().switchTo().frame(iFramePlanB);
+			return this.notificationButton;
+		}
+		if(fieldName.trim().equalsIgnoreCase("complianceLink")){
+			Web.getDriver().switchTo().defaultContent();
+			Web.getDriver().switchTo().frame(iFramePlanB);
+			return this.complianceHyperLink;
 		}
 		return null;
 	}
@@ -250,6 +304,7 @@ public class HomePage extends LoadableComponent<HomePage>{
 	}
 
 	public void logoutPSC(){
+		Web.getDriver().switchTo().defaultContent();
 		if(Web.isWebElementDisplayed(logoutLink,true)){
 			logoutLink.click();
 			Reporter.logEvent(Status.PASS,"Perform logout of PSC","Logged out of PSC successfully",false);
@@ -290,6 +345,79 @@ public class HomePage extends LoadableComponent<HomePage>{
 		return errorVerified;
 	}
 	
+	public void enterPartialPlanNumber()
+	{
+		try{
+			if(Stock.GetParameterValue("planNumber")!=null)
+			{
+				Web.setTextToTextBox(searchPlansInput, Stock.GetParameterValue("planNumber").substring(0, 5));
+				Reporter.logEvent(Status.INFO, "Check if five characters are entered in plan search input box", 
+						"Five characters from plan number are entered in plan search input box", true);
+				Thread.sleep(2000);
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean verifyAutocompletePlanSuggestion()
+	{
+		boolean autoCompleteSuggested = false;
+		if(autoCompleteSuggestionText.size()>0)
+		{
+			for(WebElement ele : autoCompleteSuggestionText)
+			{
+				if(ele.getText().contains(Stock.GetParameterValue("planNumber")))
+					autoCompleteSuggested = true;
+				Reporter.logEvent(Status.INFO,"Check if autocomplete plan suggestion is displayed", 
+						"Autocomplete plan suggestion is displayed", false);
+			}
+		}
+		else
+		{
+			Reporter.logEvent(Status.INFO, "Check if autocomplete plan suggestion is displayed", 
+					"Autocomplete plan suggestion not displayed", true);
+			autoCompleteSuggested = false;
+		}
+		return autoCompleteSuggested;
+	}
+	
+	public ResultSet getLastLoginsFromDB(String[] getLastLogins,String application, String menuFeatureCode, String user)
+	{
+		return DB.executeQuery(getLastLogins[0], getLastLogins[1], application,menuFeatureCode,"K_"+user);
+	}
+	
+	public String getLastLoginDate()
+	{
+		String lastLoginDate = "";
+		ResultSet rs = getLastLoginsFromDB(Stock.getTestQuery("getLastLogins"),Stock.GetParameterValue("application"),Stock.GetParameterValue("menu_feature_code"),Stock.GetParameterValue("username"));
+		try{
+			while(rs.next())
+			{
+				lastLoginDate = rs.getString("DPDATE");
+			}
+			if(lastLoginDate!=null)
+			{
+				Reporter.logEvent(Status.PASS, "Check last login time is stored in DB in Denver time", 
+						"Last login time is stored in DB in Denver time: "+lastLoginDate.trim(),
+						false);
+			}
+			else
+			{
+				Reporter.logEvent(Status.FAIL, "Check last login time is stored.", 
+						"Last login time is stored in DB",
+						false);
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return lastLoginDate;
+	}
+
 	
 	public void verifyHomePageMenuTabs()
 	{
@@ -314,17 +442,70 @@ public class HomePage extends LoadableComponent<HomePage>{
 				Reporter.logEvent(Status.FAIL,"Verify if all menu tabs are displayed on home page.","All menu tabs are not displayed",true);
 			}
 			
-				
-				
 		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
 		}
-	
 	}
 	
+	public String getLastLoginDateFromWeb()
+	{
+		String displayedLastLoginDate = "";
+		try
+		{
+			if(Web.isWebElementDisplayed(lastLogin, false))
+			{
+				displayedLastLoginDate = lastLogin.getText();
+				Reporter.logEvent(Status.PASS, "Check last login time displays on home page", 
+						"Last login time displays on home page: "+displayedLastLoginDate.trim().substring(11),
+						false);
+			}
+			else
+				Reporter.logEvent(Status.FAIL, "Check last login time displays on home page", 
+						"Last login time do not displays on home page", false);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return displayedLastLoginDate;
+	}
 	
+	public boolean verifyLastLoginDateEquality(String dateFromDatabase)
+	{
+		boolean equalDate = false;
+		String dateFromDB1 = dateFromDatabase.trim().substring(0,16);
+		String dateFromDB2 = dateFromDatabase.trim().substring(16);
+		String dateFromDB = dateFromDB1+" "+dateFromDB2;
+		String dateFromWeb = getLastLoginDateFromWeb().trim().substring(11,30);
+		try
+		{
+			System.out.println("Date from db is: "+dateFromDB);
+			System.out.println("Date from Web is: "+dateFromWeb);
+			SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm aaa",Locale.US);
+			Date date = sdf.parse(dateFromWeb);
+			Date date1 = sdf.parse(dateFromDB);
+			Calendar cal =Calendar.getInstance();
+			cal.setTime(date1);
+			cal.add(Calendar.HOUR, 2);
+			date1 = cal.getTime();
+			String lastLoginWeb = sdf.format(date);
+			String lastLoginDB = sdf.format(date1);
+			if(lastLoginWeb.equalsIgnoreCase(lastLoginDB))
+			{
+				equalDate = true;
+			}
+			else
+				equalDate = false;
+	
+	}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return equalDate;
+	}
 	
 	
 	public void verifySubMenuAndOptions(String menu)
@@ -440,28 +621,285 @@ public class HomePage extends LoadableComponent<HomePage>{
 		{
 			e.printStackTrace();
 		}
-		/*finally
-		{
-			expectSubMenuItems.clear();
-			actualSubMenuItems.clear();
-			actualSubSubMenuItems.clear();
-			actMenuMap.clear();
-			expMenuMap.clear();
-			
-			
-		}*/
-	
+		
 	}
+	
+	public ResultSet getSessionIDFromDB(String[] getSessionIDFromDB,String application, String menuFeatureCode, String user)
+	{
+		System.out.println("Query is:"+getSessionIDFromDB[1]);
+		return DB.executeQuery(getSessionIDFromDB[0], getSessionIDFromDB[1], application,menuFeatureCode,"K_"+user);
+	}
+	
+	public List<String> getActiveSessionIDFromDB()
+	{
+		List<String> sessionID = new ArrayList<String>();
+		try{
+			ResultSet rs = getSessionIDFromDB(Stock.getTestQuery("getSessionIDFromDB"),
+					Stock.GetParameterValue("application"),Stock.GetParameterValue("menu_feature_code"),
+					Stock.GetParameterValue("username"));
+			while(rs.next())
+			{
+				//System.out.println(rs);
+				sessionID.add(rs.getString("session_id")) ;
+			}
+			
+			if(sessionID.size()>0)
+			{
+				Reporter.logEvent(Status.INFO, "Get Session ID from DB", 
+						"Seesion ID retrieved from DB is: "+sessionID, false);
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return sessionID;
+	}
+	
+	public String getSessionIDFromBrowserCookie()
+	{
+		String cookieSessionID = null;
+		try
+		{
+			Set<Cookie> allCookies = new HashSet<Cookie>();
+			allCookies = Web.getDriver().manage().getCookies();
+			for(Cookie ck : allCookies)
+			{
+				if(ck.getName().equalsIgnoreCase("JSESSIONID")){
+					cookieSessionID = ck.getValue();
+					Reporter.logEvent(Status.INFO, "Get Session ID from browser cookie", 
+							"Seesion ID retrieved from browser cookie is: "+cookieSessionID, false);
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return cookieSessionID;
+	}
+	
+	public boolean validateCookieValue()
+	{
+		boolean equalSessionIds = false;
+		String sessionIDFromBrowserCookie;
+		try
+		{			
+			sessionIDFromBrowserCookie = getSessionIDFromBrowserCookie().trim();
+			Thread.sleep(7000);
+			for(String session : getActiveSessionIDFromDB())
+			{
+				if(sessionIDFromBrowserCookie.contains(session))
+				{
+					equalSessionIds = true;
+				}
+			}
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return equalSessionIds;
+	}
+	
+	public boolean clickOnToDoFromHome()
+	{
+		boolean clickedOnToDoFromHomePage = false;
+		try{
+			Web.getDriver().switchTo().frame(iFramePlanB);
+			lib.Web.clickOnElement(toDoButton);
+			if(Web.isWebElementDisplayed(disbursementRequest, true)){
+				Reporter.logEvent(Status.PASS, "Click on To Do from home page and check if to do list is loaded",
+						"Clicked on To Do from home page and to do list is loaded", false);
+				clickedOnToDoFromHomePage = true;
+				}
+			else
+			{
+				Reporter.logEvent(Status.FAIL, "Click on To Do from home page and check if to do list is loaded",
+						"Clicked on To Do from home page and to do list is not loaded", true);
+			}
+			Web.getDriver().switchTo().defaultContent();
+			logoutPSC();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return clickedOnToDoFromHomePage;
+	}
+	
+	public boolean clickOnNotificationFromHome()
+	{
+		boolean clickedOnNotificationFromHomePage = false;
+		try{
+			Web.getDriver().switchTo().frame(iFramePlanB);
+			Web.clickOnElement(notificationButton);
+			if(Web.isWebElementDisplayed(disbursementRequest, true)){
+				Reporter.logEvent(Status.PASS, "Click on notification from home page and check if to do list is loaded",
+						"Clicked on notification from home page and to do list is loaded", false);
+				clickedOnNotificationFromHomePage = true;
+			}
+			else
+			{
+				Reporter.logEvent(Status.PASS, "Click on notification from home page and check if to do list is loaded",
+						"Clicked on notification from home page and to do list is not loaded", true);
+			}
+			Web.getDriver().switchTo().defaultContent();
+			logoutPSC();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return clickedOnNotificationFromHomePage;
+	}
+	
+	public boolean clickOnToDoFromPlanAdministration()
+	{
+		boolean clickedOnToDoFromPlanAdmin = false;
+		try
+		{
+			Web.clickOnElement(planMenu);
+			Thread.sleep(1000);
+			Web.clickOnElement(planAdministration);
+			Thread.sleep(1000);
+			Web.clickOnElement(planAdministrationToDo);
+			Thread.sleep(1000);
+			Web.getDriver().switchTo().frame(iFramePlanB);
+			if(Web.isWebElementDisplayed(disbursementRequest, true)){
+				Reporter.logEvent(Status.PASS, "Click on to do from plan administration and check if to do list is loaded",
+						"Clicked on to do from plan administration and to do list is loaded", false);
+				clickedOnToDoFromPlanAdmin = true;
+			}
+			else
+			{
+				Reporter.logEvent(Status.PASS, "Click on to do from plan administration and check if to do list is loaded",
+						"Clicked on to do from plan administration and to do list is not loaded", true);
+			}
+			Web.getDriver().switchTo().defaultContent();
+			logoutPSC();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return clickedOnToDoFromPlanAdmin;
+	}
+	
+	public boolean verifyToDoSiteNavigation(String buttonName)
+	{
+		boolean verified = false;
+		try
+		{
+			if(buttonName.equalsIgnoreCase("toDoHomePage") && clickOnToDoFromHome())
+			{
+				verified = true;
+			}
+			else if(buttonName.equalsIgnoreCase("notificationHomePage") && clickOnNotificationFromHome())
+			{
+				verified = true;
+			}
+			else if (buttonName.equalsIgnoreCase("toDoPlanAdmin") && clickOnToDoFromPlanAdministration())
+			{
+				verified = true;
+			}
+			else
+				verified = false;
+		}
+		catch(Exception e)
+		{
+
+		}
+		return verified;
+	}
+	
+	public boolean verifySentenceCase(String buttonText)
+	{
+		buttonText = buttonText.trim();
+		boolean sentenceCase = false;
+		boolean flag = false;
+		try
+		{
+			char[] buttonTextArray = buttonText.toCharArray();
+			int i = 0;
+			for (char ch : buttonTextArray)
+			{	
+				if(i==0 && Character.isUpperCase(ch)){
+					flag = true;
+					i++;
+				}
+				else if(i!=0 && flag && !Character.isUpperCase(ch))
+				{
+					sentenceCase = true;
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return sentenceCase;
+	}
+	
+	public boolean verifyButtonTextForSentenceCase()
+	{
+		boolean isButtonTextInSentenceCase = false;
+		try{
+			Web.getDriver().switchTo().frame(iFramePlanB);
+			for(WebElement button : restyledHomeButtons)
+			{
+				if(verifySentenceCase(button.getText()))
+					isButtonTextInSentenceCase = true;
+				if(isButtonTextInSentenceCase)
+					Reporter.logEvent(Status.PASS, "Check button label "+button.getText()+" is in sentence case",
+							"Given button label is in sentence case", false);
+				else
+					Reporter.logEvent(Status.PASS, "Check button label "+button.getText()+" is in sentence case",
+							"Given button label is not in sentence case", true);
+			}
+			Web.getDriver().switchTo().defaultContent();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return isButtonTextInSentenceCase;
+	}
+	
+	public String getSecurityAnswer(String securityQuestion) {
+		String securityAnswer = null;
+		initSecurityAnsMap();
+		for (Map.Entry<String, String> entry : securityAnsMap.entrySet()) {
+			if (StringUtils.contains(securityQuestion, entry.getKey())) {
+				securityAnswer = entry.getValue();
+				break;
+			}
+		}
+		return securityAnswer;
+	}
+
+	private void initSecurityAnsMap() {
+		String secAns = Stock.getConfigParam("SecurityAns_"
+	                   +Stock.getConfigParam("TEST_ENV"));
+		if (securityAnsMap == null & !secAns.isEmpty()) {
+			securityAnsMap = new LinkedHashMap<String, String>();
+			for (String QA : secAns.split("\\|")) {
+				securityAnsMap.put(QA.split(",")[0].trim(),
+						QA.split(",")[1].trim());
+			}
+		}
+	}
+	
 	
 	public boolean searchPlanWithName()
 	{ boolean planTextDisplayed = false;
 	try{
 		Web.setTextToTextBox(searchPlansInput, Stock.GetParameterValue("planName"));
-		
-			Web.clickOnElement(searchPlanButton);
-			Web.isWebElementDisplayed(moreButton, true);
-			if(Stock.GetParameterValue("planName")!=null)
-			if(planHeaderInfo.getText().contains(Stock.GetParameterValue("planName")))
+		Web.clickOnElement(searchPlanButton);
+		Web.isWebElementDisplayed(moreButton, true);
+		if(Stock.GetParameterValue("planName")!=null)
+		if(planHeaderInfo.getText().contains(Stock.GetParameterValue("planName")))
 				return planTextDisplayed = true;
 	}
 	catch(Exception e)
@@ -513,7 +951,7 @@ public class HomePage extends LoadableComponent<HomePage>{
 	
 	public void jumpPageVerificationWhenPlanAccessInSingleSite() throws Exception
 	{
-		AccountVerificationPage act = new AccountVerificationPage();
+		//AccountVerificationPage act = new AccountVerificationPage();
 		if(this.isJumpPageDisplayed())
 		{
 			Reporter.logEvent(Status.FAIL,"Verify if Jump page is not displayed if user has access to plans only in single site","Jump page is displayed.",true);
@@ -526,7 +964,7 @@ public class HomePage extends LoadableComponent<HomePage>{
 	
 	public void jumpPageVerificationWhenPlanAccessInAllSite() throws Exception
 	{
-		AccountVerificationPage act = new AccountVerificationPage();
+		//AccountVerificationPage act = new AccountVerificationPage();
 		if(this.isJumpPageDisplayed())
 		{
 			Reporter.logEvent(Status.PASS,"Verify if Jump page is displayed if user has access to plans in all sites.","Jump page is displayed.",false);
@@ -585,22 +1023,5 @@ public class HomePage extends LoadableComponent<HomePage>{
 			Web.clickOnElement(menuElement(specifiedTab[0]));	
 		}
 	}
-	
-	
-	
-	
-	
-	
-
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
