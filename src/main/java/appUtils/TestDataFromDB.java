9a7b7c3808db26d648230f6fbcfaef8e920af537
srsksr <sireesha.kosaraju@greatwest.com>
2018-02-07 12:12:11 +0530

    Adds test data for deferral TC

--- src/main/java/appUtils/TestDataFromDB.java
+++ src/main/java/appUtils/TestDataFromDB.java
@@ -20,7 +20,7 @@ public class TestDataFromDB {
 	static LinkedHashMap<Long, LinkedHashMap<String, String>> testdataFromDB = new LinkedHashMap<Long, LinkedHashMap<String, String>>();
 	static ResultSet participants ;
 	static boolean queryExecuted=false;
-	static int j=3;
+	static int j=2;
 	
 	static ResultSetMetaData rsMetaData = null;
 	@SuppressWarnings("null")
